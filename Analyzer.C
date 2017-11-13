b2f3baecfbf066a75caa54fa4971fd67d195fa6c
Andres Vargas <vargasa@cua.edu>
2017-11-13 15:53:19 -0500

    Convert CSV to ROOT File and plot some peak prices histogram

---
+++ Analyzer.C
@@ -0,0 +1,53 @@
+#define XMIN -0.5
+#define XMAX 0.5
+#define NBINS 100
+
+int Analyzer(){
+
+  TString fStartDate = "Jan 01 2009";
+  TString fEndDate = "Nov 10 2017";
+  TString fSymbol = "SOXL";
+  TString fFreq = "1wk";  // 1d, 1wk, 1mo
+  gSystem->Exec("sh getData.sh "+fSymbol+" "+fFreq+" '"+fStartDate+"' "+"'"+fEndDate+"'");
+
+  TFile *f = new TFile(fSymbol+".root","RECREATE");
+  TTree *tree = new TTree(fSymbol,"From CSV File");
+  tree->ReadFile(fSymbol+".csv","fDate/C:fOpen/F:fHigh/F:fLow/F:fClose/F:fCloseAdj/F:fVolume/I",',');
+  f->Write();
+
+  TTreeReader fReader(fSymbol, f);
+  TTreeReaderValue<Float_t> fH(fReader,"fHigh");
+  TTreeReaderValue<Float_t> fL(fReader,"fLow");
+  TTreeReaderValue<Float_t> fO(fReader,"fOpen");
+  TTreeReaderValue<Float_t> fC(fReader,"fClose");
+
+  TH1F *fHDiffLL = new TH1F("fHDiffLL",fSymbol+";Difference between one-day low prices;Counts",NBINS,XMIN,XMAX);
+  TH1F *fHDiffHH = new TH1F("fHDiffHH",fSymbol+";Difference between one-day high prices;Counts",NBINS,XMIN,XMAX);
+  TH1F *fHDiffLH = new TH1F("fHDiffLH",fSymbol+";Difference between "+fFreq+" high and previous "+fFreq+" low prices;Counts",NBINS,XMIN,XMAX);
+
+  Float_t fPrevL;
+  Float_t fPrevH;
+   
+  while(fReader.Next()){
+    fHDiffLL->Fill( (*fL - fPrevL)/(fPrevL) );
+    fHDiffLH->Fill( (*fH - fPrevL)/(fPrevL) );
+    fHDiffHH->Fill( (*fH - fPrevH)/(fPrevH) );
+    fPrevL = *fL;
+    fPrevH = *fH;		    
+  }
+  
+  TCanvas *c1 = new TCanvas("c1");
+  c1->Divide(2,2);
+  
+  c1->cd(1);
+  fHDiffLH->Draw("HIST");
+
+  c1->cd(2);
+  fHDiffLL->Draw("HIST");
+
+  c1->cd(3);
+  fHDiffHH->Draw("HIST");
+  
+  return 0;
+
+}
