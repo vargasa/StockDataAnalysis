28a210773a89227f4d92151c7bf211f14f880b9e
Andres Vargas <vargasa@jlab.org>
2017-11-19 11:29:36 -0500

    First guess NLP = LP*(1+MeanDifferenceLL(HD)) 	    NHP = HP*(1+MeanDifferenceHH(HD))

--- Analyzer.C
+++ Analyzer.C
@@ -2,13 +2,13 @@
 #define XMAX 0.5
 #define NBINS 100
 
-int Analyzer(){
+int Analyzer( TString fSymbol = "SOXL",
+	      TString fFreq = "1wk",
+	      TDatime fStartDate = TDatime("2010-01-01 00:00:00"),
+	      TDatime fEndDate = TDatime("2017-11-10 00:00:00") ) {
 
-  TString fStartDate = "Nov 10 2016";
-  TString fEndDate = "Nov 10 2017";
-  TString fSymbol = "SOXL";
-  TString fFreq = "1wk";  // 1d, 1wk, 1mo
-  gSystem->Exec("sh getData.sh "+fSymbol+" "+fFreq+" '"+fStartDate+"' "+"'"+fEndDate+"'");
+  // 1d, 1wk, 1mo
+  gSystem->Exec("sh getData.sh "+fSymbol+" "+fFreq+" '"+fStartDate.AsString()+"' "+"'"+fEndDate.AsString()+"'");
 
   TFile *f = new TFile(fSymbol+".root","RECREATE");
   TTree *tree = new TTree(fSymbol,"From CSV File");
@@ -16,25 +16,83 @@ int Analyzer(){
   f->Write();
 
   TTreeReader fReader(fSymbol, f);
+  TTreeReaderArray<char> fDt(fReader,"fDate");
   TTreeReaderValue<Float_t> fH(fReader,"fHigh");
   TTreeReaderValue<Float_t> fL(fReader,"fLow");
   TTreeReaderValue<Float_t> fO(fReader,"fOpen");
   TTreeReaderValue<Float_t> fC(fReader,"fClose");
 
-  TH1F *fHDiffLL = new TH1F("fHDiffLL",fSymbol+";Difference between "+fFreq+" high and previous "+fFreq+" low prices;Counts from "+fStartDate+" to "+fEndDate,NBINS,XMIN,XMAX);
-  TH1F *fHDiffHH = new TH1F("fHDiffHH",fSymbol+";Difference between "+fFreq+" high and previous "+fFreq+" high prices;Counts from "+fStartDate+" to "+fEndDate,NBINS,XMIN,XMAX);
-  TH1F *fHDiffLH = new TH1F("fHDiffLH",fSymbol+";Difference between "+fFreq+" high and previous "+fFreq+" low prices;Counts from "+fStartDate+" to "+fEndDate,NBINS,XMIN,XMAX);
+  TH1F *fHDiffLL = new TH1F("fHDiffLL",
+			    fSymbol+";Difference between "+fFreq+" high and previous "
+			    +fFreq+" low prices;Counts from "+fStartDate.AsString()
+			    +" to "+fEndDate.AsString(),
+			    NBINS,XMIN,XMAX);
+  TH1F *fHDiffHH = new TH1F("fHDiffHH",
+			    fSymbol+";Difference between "+fFreq+" high and previous "
+			    +fFreq+" high prices;Counts from "+fStartDate.AsString()
+			    +" to "+fEndDate.AsString(),
+			    NBINS,XMIN,XMAX);
+  TH1F *fHDiffLH = new TH1F("fHDiffLH",
+			    fSymbol+";Difference between "+fFreq+" high and previous "
+			    +fFreq+" low prices;Counts from "+fStartDate.AsString()
+			    +" to "+fEndDate.AsString(),
+			    NBINS,XMIN,XMAX);
 
   Float_t fPrevL;
   Float_t fPrevH;
-   
+  
+  Int_t fEvent = 0;
+
+  Float_t fNPriceHH;
+  Float_t fNPriceLL;
+  
+  Int_t fFail = 0;
+  Int_t fSuccess = 0;
+     
   while(fReader.Next()){
+
+    if ( fEvent > 105 ) {
+      
+      if ( (fNPriceLL > *fL) && (fNPriceHH < *fH) ) {
+	fSuccess++;
+	printf("||======SUCESS======||\n");
+      } else {
+	fFail++;
+	printf("||=====FAILURE======||\n");
+      }
+      printf("Prediction LP: %f ; Prediction HP : %f\n", fNPriceLL, fNPriceHH);
+      printf("Actual LP: %f ; Actual HP: %f\n", *fL,*fH);
+      printf("Difference LP: %.2f%% Difference HP: %.2f%%\n", (*fL-fNPriceLL)*100/fNPriceLL, (*fH-fNPriceHH)*100/fNPriceHH);
+      
+
+    }
+    
+    fEvent++;
     fHDiffLL->Fill( (*fL - fPrevL)/(fPrevL) );
     fHDiffLH->Fill( (*fH - fPrevL)/(fPrevL) );
     fHDiffHH->Fill( (*fH - fPrevH)/(fPrevH) );
+    
     fPrevL = *fL;
-    fPrevH = *fH;		    
+    fPrevH = *fH;
+    
+    if ( fEvent > 104 ) {
+      
+      TF1 *fGausLL = new TF1("GausLL","gaus");
+      fHDiffLL->Fit("GausLL","QM+");
+      fNPriceLL = fPrevL*(1+fGausLL->GetParameter(1));
+      //fHDiffLL->Reset();
+      
+      TF1 *fGausHH = new TF1("GausHH","gaus");
+      fHDiffHH->Fit("GausHH","QM+");
+      fNPriceHH = fPrevH*(1 + fGausHH->GetParameter(1));
+      //fHDiffHH->Reset();
+	
+    } 
+ 
   }
+
+  printf("Success:%d Failures:%d\n",fSuccess,fFail);
+  
   
   TCanvas *c1 = new TCanvas("c1","c1",2048,1152);
   c1->Divide(2,2);
@@ -51,14 +109,14 @@ int Analyzer(){
   TF1 *fGausLL = new TF1("GausLL","gaus");
   fHDiffLL->Fit("GausLL","QM+");
   Float_t fHPriceLL = fPrevL*(1+fGausLL->GetParameter(1));
-  printf("Next Low price: %f\n",fHPriceLL);
+  //printf("Next Low price: %f\n",fHPriceLL);
   fHDiffLL->Draw();
 
   c1->cd(3);
   TF1 *fGausHH = new TF1("GausHH","gaus");
   fHDiffHH->Fit("GausHH","QM+");
   Float_t fHPriceHH = fPrevH*(1 + fGausHH->GetParameter(1));
-  printf("Next High price: %f\n",fHPriceHH);
+  //printf("Next High price: %f\n",fHPriceHH);
   fHDiffHH->Draw();
   c1->Print(fSymbol+".png");
   
