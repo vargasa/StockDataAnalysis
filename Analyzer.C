b70636ed685bee442dd74f86e2f0e50081a490fb
Andres Vargas <vargasa@jlab.org>
2017-11-23 09:03:11 -0500

    Organizing different functions for SMA, GetData and Derivative

--- Analyzer.C
+++ Analyzer.C
@@ -2,7 +2,14 @@
 #define XMAX 0.5
 #define NBINS 100
 
+TString gSymbol;
+
+////////////////////////////////////////////////////////////////////////////////
+/// Function to compute an index when using present and historical data when
+/// using composed variables
+
 Int_t GetIndex( Int_t fEvent, Int_t fInterval){
+
 
   Int_t fIndex = 0;
   Int_t fAux = (fEvent%fInterval - 1);
@@ -16,7 +23,10 @@ Int_t GetIndex( Int_t fEvent, Int_t fInterval){
     
 }
 
-Int_t GetData( TString fSymbol,
+////////////////////////////////////////////////////////////////////////////////
+/// Get data from Yahoo! Finance API using getData.sh Shell Script
+
+TFile *GetData( TString fSymbol,
 	       TString fFreq,
 	       TDatime fStartDate,
 	       TDatime fEndDate){
@@ -27,18 +37,87 @@ Int_t GetData( TString fSymbol,
   TTree *tree = new TTree(fSymbol,"From CSV File");
   tree->ReadFile(fSymbol+".csv","fDate/C:fOpen/F:fHigh/F:fLow/F:fClose/F:fCloseAdj/F:fVolume/I",',');
   f->Write();
-  f->Close();
 
-  return 0;
+  return f;
 
 }
 
-int Analyzer( TString fSymbol = "SOXL",
+////////////////////////////////////////////////////////////////////////////////
+/// Simple Moving Average SMA
+
+TGraph *GetSMA(TFile *f,
+	       Int_t fInterval = 6,
+	       Option_t *Option="close"){
+  
+  TTreeReader fReader(gSymbol, f);
+  TTreeReaderArray<char> fDt(fReader,"fDate");
+  TTreeReaderValue<Float_t> fH(fReader,"fHigh");
+  TTreeReaderValue<Float_t> fL(fReader,"fLow");
+  TTreeReaderValue<Float_t> fO(fReader,"fOpen");
+  TTreeReaderValue<Float_t> fC(fReader,"fClose");
+
+  TDatime fDate;
+  Int_t fEvent = 0;
+
+  Float_t fPrice[fInterval];
+  TGraph *fGSMA = new TGraph();
+
+  while(fReader.Next()){
+
+    TString fSDt = static_cast<char*>(fDt.GetAddress());
+    fSDt.Append(" 00:00:00");
+    fDate = TDatime(fSDt);
+    
+    fPrice[GetIndex(fEvent,fInterval)] = *fC;
+
+    if (fEvent > fInterval){
+      Float_t SMA = TMath::Mean(fInterval,&fPrice[0]);     
+      fGSMA->SetPoint(fGSMA->GetN(),fDate.Convert(),SMA);
+    }
+    fEvent++;
+    
+  }
+  fGSMA->SetTitle(Form("%s SMA(%d);Date;SMA",gSymbol.Data(),fInterval));
+  fGSMA->GetXaxis()->SetTimeDisplay(1);
+  fGSMA->GetXaxis()->SetTimeFormat("%Y/%m/%d");
+  fGSMA->GetXaxis()->SetTimeOffset(0,"gmt");
+
+  return fGSMA;
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Numerical derivative working only for equaly distant x-data 
+
+TGraph *Derivative(TGraph *fg){
+  
+  TGraph *fgd = new TGraph();
+
+  for (Int_t i = 1; i < fg->GetN(); i++){
+    Double_t x1,y1,x2,y2;
+    fg->GetPoint(i-1,x1,y1);
+    fg->GetPoint(i,x2,y2);
+    Double_t dy = y2 - y1;
+    fgd->SetPoint(i-1,x2,dy);
+  }
+  
+  fgd->GetXaxis()->SetTimeDisplay(1);
+  fgd->GetXaxis()->SetTimeFormat("%Y/%m/%d");
+  fgd->GetXaxis()->SetTimeOffset(0,"gmt");
+
+  return fgd;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Main Function
+Int_t Analyzer( TString fSymbol = "SOXL",
 	      TString fFreq = "1wk",
 	      TDatime fStartDate = TDatime("2009-01-01 00:00:00"),
 	      TDatime fEndDate = TDatime("2017-11-17 00:00:00") ) {
 
-  TFile *f = new TFile(fSymbol+".root","OPEN");
+  TFile *f = GetData(fSymbol, fFreq, fStartDate, fEndDate);
+  f->ReOpen("READ");
+  gSymbol = fSymbol;
 
   TTreeReader fReader(fSymbol, f);
   TTreeReaderArray<char> fDt(fReader,"fDate");
@@ -70,15 +149,6 @@ int Analyzer( TString fSymbol = "SOXL",
   
   Int_t fEvent = 0;
 
-  // Fast and Slow Moving Averages
-  TString fSMAOption = "close";
-  Int_t fFastSMAInterval = 7;
-  Int_t fSlowSMAInterval = 26;
-  Float_t fPriceFastSMA[fFastSMAInterval];
-  Float_t fPriceSlowSMA[fSlowSMAInterval];
-  TGraph *fGFastSMA = new TGraph();
-  TGraph *fGSlowSMA = new TGraph();
-
   Float_t fNPriceHH;
   Float_t fNPriceLL;
   
@@ -88,18 +158,6 @@ int Analyzer( TString fSymbol = "SOXL",
     fSDt.Append(" 00:00:00");
     fDate = TDatime(fSDt);
 
-    fPriceFastSMA[GetIndex(fEvent,fFastSMAInterval)] = *fC;
-    fPriceSlowSMA[GetIndex(fEvent,fSlowSMAInterval)] = *fC;
-
-    if (fEvent > fSlowSMAInterval){
- 
-      Float_t FSMA = TMath::Mean(fFastSMAInterval,&fPriceFastSMA[0]);
-      Float_t SSMA = TMath::Mean(fSlowSMAInterval,&fPriceSlowSMA[0]);
-      
-      fGFastSMA->SetPoint(fGFastSMA->GetN(),fDate.Convert(),FSMA);
-      fGSlowSMA->SetPoint(fGSlowSMA->GetN(),fDate.Convert(),SSMA);
-    }
-    
     fEvent++;
     fHDiffLL->Fill( (*fL - fPrevL)/(fPrevL) );
     fHDiffLH->Fill( (*fH - fPrevL)/(fPrevL) );
@@ -129,35 +187,21 @@ int Analyzer( TString fSymbol = "SOXL",
   //printf("Next Low price: %f\n",fHPriceLL);
   fHDiffLL->Draw();
 
+  
   c1->cd(3);
+  TGraph *fGSlowSMA = GetSMA(f, 10,"close");
+  fGSlowSMA->Draw("AL");
+
+  c1->cd(4);
   TF1 *fGausHH = new TF1("GausHH","gaus");
   fHDiffHH->Fit("GausHH","QM+");
   Float_t fHPriceHH = fPrevH*(1 + fGausHH->GetParameter(1));
+  TGraph *fGdSlowSMA = Derivative(fGSlowSMA);
+  fGdSlowSMA->SetTitle(Form("%s;Date;SlowSMA Derivative",fSymbol.Data()));
+  fGdSlowSMA->Draw("AL*");
+  
   //printf("Next High price: %f\n",fHPriceHH);
   //fHDiffHH->Draw();
-  
-  TGraph *fGdSSMA = new TGraph();
-
-  for (Int_t i = 1; i < fGSlowSMA->GetN(); i++){
-    Double_t x1,y1,x2,y2;
-    fGSlowSMA->GetPoint(i-1,x1,y1);
-    fGSlowSMA->GetPoint(i,x2,y2);
-    Double_t dy = y2 - y1;
-    fGdSSMA->SetPoint(i-1,x2,dy/y1);
-  }
-  fGdSSMA->SetTitle(fSymbol+" Slow SMA Derivative; Date; Price");
-  fGdSSMA->GetXaxis()->SetTimeDisplay(1);
-  fGdSSMA->GetXaxis()->SetTimeFormat("%Y/%m/%d");
-  fGdSSMA->GetXaxis()->SetTimeOffset(0,"gmt");
-  fGdSSMA->Draw("AL*");
-  
-  c1->cd(4);
-  fGFastSMA->SetTitle(fSymbol+" Fast SMA; Date; Price");
-  fGFastSMA->GetXaxis()->SetTimeDisplay(1);
-  fGFastSMA->GetXaxis()->SetTimeFormat("%Y/%m/%d");
-  fGFastSMA->Draw("AP");
-  fGSlowSMA->Draw("P SAME");
-  fGdSSMA->Draw("l");
      
   c1->Print(fSymbol+".png");
 
