35033d567d101f387e273eeb82e65f6cd9e4dcef
Andres Vargas <vargasa@cua.edu>
2017-12-04 22:46:45 -0500

    Script to get All Symbols added

---
+++ getAll.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {NYSE,NASDAQ,AMEX}
+do
+    while read symbol; do
+	root -l -q -b 'Analyzer.C("'$symbol'","1wk","2009-01-01 00:00:00","2017-11-30 00:00:00")'
+    done < Symbols/$i.txt
+done
