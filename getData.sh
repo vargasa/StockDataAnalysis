82078d869253cfa45ac75fbed092dd0b21d10a46
Andres Vargas <vargasa@jlab.org>
2017-12-09 11:12:11 -0500

    Retry (3 times) to download data if failed

--- getData.sh
+++ getData.sh
@@ -16,7 +16,7 @@ last_date=$(date -d "$4" '+%s')
 
 alias wget='wget -q --no-check-certificate'
 
-wget --save-cookies=/tmp/ycookie.txt https://finance.yahoo.com/quote/$symbol/?p=$symbol -O /tmp/ycrumb.store
+wget --tries=3 --save-cookies=/tmp/ycookie.txt https://finance.yahoo.com/quote/$symbol/?p=$symbol -O /tmp/ycrumb.store
 
 crumb=$(grep 'root.*App' /tmp/ycrumb.store | sed 's/,/\n/g' | grep CrumbStore | sed 's/"CrumbStore":{"crumb":"\(.*\)"}/\1/')
 
