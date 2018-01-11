453c7236c22b47401de3e93c6daabb8d9394d95e
Andres Vargas <vargasa@jlab.org>
2018-01-11 11:13:00 -0500

    * getAll.sh now can be used with the new structure of Analizer.C

--- getAll.sh
+++ getAll.sh
@@ -2,7 +2,5 @@
 
 for i in {NYSE,NASDAQ,AMEX}
 do
-    while read symbol; do
-	root -l -q -b 'Analyzer.C("'$symbol'","1wk","2009-01-01 00:00:00","2017-12-10 00:00:00",'$1')'
-    done < Symbols/$i.txt
+    root -l -b -q 'Analyzer.C("Symbols/'$i'.txt")'
 done
