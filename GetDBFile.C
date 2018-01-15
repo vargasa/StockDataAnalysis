1e15a142d74c1e36470f266bf73b3ab043d0cc83
Andres Vargas <vargasa@jlab.org>
2018-01-15 12:35:24 -0500

    * GetDBFile.C Allows to download data to a ROOTFile 	  which can be used as a LocalDB

---
+++ GetDBFile.C
@@ -0,0 +1,34 @@
+#include "TStock.h"
+
+Int_t GetDBFile(TString listfile = "Symbols/NASDAQ.txt", TString Freq = "1wk", TString StartDate = "2000-01-01 00:00:00") {
+
+  gErrorIgnoreLevel = 2001;
+
+  TString filename = "SymbolsDB";
+  std::ifstream infile(listfile.Data());
+
+  std::string line;
+
+  TFile *fOut = new TFile("Output/"+filename+".root","RECREATE");
+
+  while (std::getline(infile, line)){
+
+    TString Symbol = TString(line);
+    TStock *Stock = new TStock(Symbol,Freq,StartDate);
+    cout << Symbol+"\t";
+    TTree *tree = Stock->GetData();
+    if(tree){
+      tree->SetName(Form("%s_%s",Symbol.Data(),Freq.Data()));
+      tree->Write();
+      cout << "[Ok]\n";
+    } else {
+      cout << "[Error]\n";
+    }
+    
+  }
+  
+  fOut->Close();
+
+  return 0;
+
+}
